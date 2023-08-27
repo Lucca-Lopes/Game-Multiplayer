bf0f62a99ba0f00dabdf0ee791cbf14e2b1f5025
HugoCorrea20 <61765713+HugoCorrea20@users.noreply.github.com>
2023-08-27 00:35:32 -0300

    interação

--- Assets/Script/Personagem.cs
+++ Assets/Script/Personagem.cs
@@ -6,17 +6,36 @@ using UnityEngine.InputSystem;
 public class Personagem : MonoBehaviour
 {
     private Rigidbody rb;
-    private Vector2 movimento; 
+    private Vector2 movimento;
+    private InputAction interactAction;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        interactAction = new InputAction("Interact", binding: "<Keyboard>/e");
+        interactAction.performed += Interact;
     }
 
     public void SetMovimento(InputAction.CallbackContext value)
     {
         movimento = value.ReadValue<Vector2>();
     }
+    private void OnEnable()
+    {
+        interactAction.Enable();
+    }
+    private void OnDisable()
+{
+    interactAction.Disable();
+}
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            //SeuMetodoDeInteracao(); 
+        }
+    }
+
     /*
     public void SetPular(InputAction.CallbackContext value)
     {
