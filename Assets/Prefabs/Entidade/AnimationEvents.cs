903372739e6d9b2b3fb7e0025f1a49b63dbbfe7e
Lucca-Lopes <luccavlopes@hotmail.com>
2023-09-24 13:45:16 -0300

    feat: coisas da ariel adicionadas

---
+++ Assets/Prefabs/Entidade/AnimationEvents.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class AnimationEvents : MonoBehaviour
+{
+    public Animator anim;
+    public bool andando;
+    public bool levantandoCrianca;
+    public bool soltandoCrianca;
+    public bool usandoHabilidade;
+    public bool atacando;
+
+    public VisualEffect direito;
+    public VisualEffect esquerdo;
+    public void PlayVisualEffects(int pe)
+    {
+        if(pe == 1)
+        {
+            esquerdo.Play();
+        }
+        else if(pe == 2)
+        {
+            direito.Play();
+        }
+    }
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        anim.SetBool("andando", andando);
+        anim.SetBool("levantandoCrianca", levantandoCrianca);
+        anim.SetBool("soltandoCrianca", soltandoCrianca);
+        anim.SetBool("usandoHabilidade", usandoHabilidade);
+        anim.SetBool("atacando", atacando);
+    }
+}
