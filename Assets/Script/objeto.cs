8be018a9cad9ae4efe67cbb27045e7c9e57d07c7
HugoCorrea20 <61765713+HugoCorrea20@users.noreply.github.com>
2023-10-06 15:41:50 -0300

    ajustes

--- Assets/Script/objeto.cs
+++ Assets/Script/objeto.cs
@@ -10,13 +10,18 @@ public class objeto : MonoBehaviour
     private Personagem jogador;
     public float fillRate = 0.05f;
     public float interactionProgress = 0f;
-    public  bool isInteracting = false;
+    public bool isInteracting = false;
     public bool wasInteractingBeforeMoving = false;
     public bool isAnyPlayerInteracting = false;
-    private bool isInteractionAvailable = true; 
+    private bool isInteractionAvailable = true;
     public float maxInteractionDistance = 3f;
+    private float interactionDuration = 90f; // Defina a duração da interação em segundos
+    private float currentTime = 0f; // Tempo atual da interação
+    private float pausedInteractionProgress = 0f;
+    private bool wasInteractingBeforePause = false;
 
     private GameObject jogadors;
+
     public void SetJogador(Personagem player)
     {
         jogador = player;
@@ -28,7 +33,6 @@ public class objeto : MonoBehaviour
     void Start()
     {
         jogadors = GameObject.Find("Player");
-
     }
 
     public void StartInteraction()
@@ -36,27 +40,40 @@ public class objeto : MonoBehaviour
         if (isInteractionAvailable)
         {
             Debug.Log("Iniciando interação...");
-            if (progressBar != null)
+            progressBar.gameObject.SetActive(true);
+
+            // Verifique se o jogador estava interagindo antes da pausa
+            if (wasInteractingBeforePause)
             {
-                progressBar.gameObject.SetActive(true);
+                // Se estava, retome a partir do progresso salvo
+                interactionProgress = pausedInteractionProgress;
+                progressBar.value = interactionProgress;
+            }
+            else
+            {
+                // Se não estava, comece do início
+                interactionProgress = 0f;
             }
 
-
-            interactionProgress = 0f;
-
-
             isInteracting = true;
-           wasInteractingBeforeMoving = true;
-           isAnyPlayerInteracting = true;
+            wasInteractingBeforeMoving = true;
+            isAnyPlayerInteracting = true;
         }
         else
         {
             Debug.Log("A interação não está disponível.");
         }
     }
+
+
     public void StopInteraction()
     {
         Debug.Log("Interação interrompida pelo jogador.");
+
+        // Salve o progresso da interação antes de interromper
+        pausedInteractionProgress = interactionProgress;
+        wasInteractingBeforePause = wasInteractingBeforeMoving;
+
         progressBar.gameObject.SetActive(false);
         interactionProgress = 0f;
         isInteracting = false;
@@ -64,67 +81,53 @@ public class objeto : MonoBehaviour
         isAnyPlayerInteracting = false;
     }
 
+
     public void ResumeInteraction()
     {
         Debug.Log("Retomando interação...");
         progressBar.gameObject.SetActive(true);
-
-
         isInteracting = true;
         isAnyPlayerInteracting = true;
         wasInteractingBeforeMoving = true;
-
-
-       
     }
 
     void Update()
     {
-
-
         if (isInteracting)
         {
             if (!jogador || !jogador.isMoving)
             {
+                currentTime += Time.deltaTime;
 
-
-                interactionProgress += Time.deltaTime * fillRate;
-
+                // Calcule o progresso da interação com base no tempo atual
+                interactionProgress = currentTime / interactionDuration;
 
                 interactionProgress = Mathf.Clamp01(interactionProgress);
-
-
                 progressBar.value = interactionProgress;
 
-                if (interactionProgress >= 1f)
+                if (currentTime >= interactionDuration)
                 {
-
                     Debug.Log("Interação concluída!");
-
-
                     isInteracting = false;
                     progressBar.gameObject.SetActive(false);
                     interactionProgress = 0f;
-
-
                     objectsCompleted += 1;
-                    GameManager.Instance.objectsCompleted.Value+=1;
-
-
+                    GameManager.Instance.objectsCompleted.Value += 1;
                     isInteractionAvailable = false;
                 }
             }
             else
             {
-
                 Debug.Log("Interação interrompida devido ao movimento.");
                 progressBar.gameObject.SetActive(false);
                 interactionProgress = 0f;
                 isInteracting = false;
-               wasInteractingBeforeMoving = false;
+                wasInteractingBeforeMoving = false;
                 isAnyPlayerInteracting = false;
+
+                // Redefina o tempo atual
+                currentTime = 0f;
             }
         }
     }
-
 }
