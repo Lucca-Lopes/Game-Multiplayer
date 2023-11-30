46cead223167664c921b1287be5770110d3ce04e
Ariel <129689424+arii-mms@users.noreply.github.com>
2023-11-30 19:27:58 -0300

    FODASE FUNCIONA
    MANAGE TODO REFEITO SE QUISE EU EXPLICO A LOGICA DEPOIS

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.Netcode;
+using System;
 
 public class SpawnManager : NetworkBehaviour
 {
@@ -13,34 +14,72 @@ public class SpawnManager : NetworkBehaviour
     public int jogadoresEsperados = 4;
     public NetworkVariable<int> jogadoresConectados = new(0);
 
+    private void Awake()
+    {
+        jogadoresConectados.OnValueChanged += JogadoresContectadosHandler;
+    }
+
+    private void JogadoresContectadosHandler(int previousValue, int newValue)
+    {
+        Debug.Log($"{newValue} jogadores conectados");
+        if (jogadoresEsperados == newValue)
+        {
+            if (IsHost)
+            {
+                SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId, true);
+            }
+            else if (IsClient)
+            {
+                SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId, false);
+            }
+            if (IsServer)
+            {
+                InvokeRepeating(nameof(UpdateTimerActive_ServerRpc), 3f, 1f);
+            }
+        }
+            //Invoke(nameof(UpdateTimerActive_ServerRpc), 50f);
+    }
+
     public override void OnNetworkSpawn()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        if (IsHost)
+        /*if (IsHost)
         {
             SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId, true);
         }
         else if (IsClient)
         {
             SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId, false);
-        }
-        AdicionarPlayerConectado_ServerRpc(NetworkManager.Singleton.LocalClientId);
-        Invoke(nameof(UpdateTimerActive_ServerRpc), 20f);
+        }*/
+        AdicionarPlayerConectado_ServerRpc();
+        //Invoke(nameof(UpdateTimerActive_ServerRpc), 50f);
         base.OnNetworkSpawn();
     }
 
     [ServerRpc(RequireOwnership = false)]
-    public void AdicionarPlayerConectado_ServerRpc(ulong clientId)
+    public void AdicionarPlayerConectado_ServerRpc()
     {
         jogadoresConectados.Value += 1;
+        Debug.Log($"{jogadoresConectados.Value} jogadores conectados");
     }
 
     [ServerRpc(RequireOwnership = false)]
     public void UpdateTimerActive_ServerRpc()
     {
-        if (jogadoresEsperados <= jogadoresConectados.Value)
+        //foreach (int id in jogadoresConectados.Value)
+        for(int i = 0; i < jogadoresEsperados; i++)
+        {
+            if (NetworkManager.Singleton.ConnectedClients[(ulong)i].PlayerObject == null)
+                return;
+        }
+        GameManager.Instance.timerAtivo.Value = true;
+
+        /*if (jogadoresEsperados == jogadoresConectados.Value)
+        {
             GameManager.Instance.timerAtivo.Value = true;
+            Debug.Log($"Iniciando timer - Server side");
+        }*/
     }
 
     [ServerRpc(RequireOwnership = false)]
